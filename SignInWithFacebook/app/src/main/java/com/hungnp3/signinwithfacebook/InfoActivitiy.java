ca50525d6a3b9adb1534e928cddd3044eb0fac8b
hung.nguyen1 <hung.nguyen1@ntq-solution.com.vn>
2019-02-22 14:21:42 +0700

    Share Link, Share Image from Resource

--- SignInWithFacebook/app/src/main/java/com/hungnp3/signinwithfacebook/InfoActivitiy.java
+++ SignInWithFacebook/app/src/main/java/com/hungnp3/signinwithfacebook/InfoActivitiy.java
@@ -1,21 +1,35 @@
 package com.hungnp3.signinwithfacebook;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
 
-public class InfoActivitiy extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfoActivitiy extends AppCompatActivity implements View.OnClickListener {
     private ProfilePictureView ppvAvatar;
     private TextView tvName, tvBirthday, tvEmail;
     private LoginButton btnLogout;
+    private ShareDialog shareDialog;
+    private ShareLinkContent shareLinkContent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +43,11 @@ public class InfoActivitiy extends AppCompatActivity {
         tvName = findViewById(R.id.tv_user_name);
         tvBirthday = findViewById(R.id.tv_birthday);
         tvEmail = findViewById(R.id.tv_email);
+
+        shareDialog = new ShareDialog(this);
+
+        findViewById(R.id.btn_share_link).setOnClickListener(this);
+        findViewById(R.id.btn_share_img).setOnClickListener(this);
 
         FacebookManager.getInstance().loadLoginInfo(new FacebookManager.OnLoadInfoCallBack() {
             @Override
@@ -60,9 +79,7 @@ public class InfoActivitiy extends AppCompatActivity {
                 Log.d("MY_LOG", "onCurrentAccessTokenChanged()");
                 Log.d("MY_LOG", "accessToken: " + accessToken);
                 Log.d("MY_LOG", "accessToken2: " + accessToken2);
-                if (accessToken == null) {
-                    // Log in Logic
-                } else if (accessToken2 == null) {
+                if (accessToken2 == null) {
                     // Log out logic
                     startActivity(new Intent(InfoActivitiy.this, MainActivity.class));
                     finish();
@@ -70,5 +87,50 @@ public class InfoActivitiy extends AppCompatActivity {
             }
         };
         accessTokenTracker.startTracking();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_share_link:
+                shareLink();
+                break;
+            case R.id.btn_share_img:
+                shareImage();
+                break;
+        }
+    }
+
+    private void shareImage() {
+        // Image from resource
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.test_img);
+        //
+
+        
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(image)
+                .build();
+        List<SharePhoto> listPhotos = new ArrayList<>();
+        listPhotos.add(photo);
+        listPhotos.add(photo);
+        listPhotos.add(photo);
+        listPhotos.add(photo);
+        listPhotos.add(photo);
+        listPhotos.add(photo);
+        SharePhotoContent sharePhotoContent = new SharePhotoContent.Builder()
+//                .addPhoto(photo)
+                .addPhotos(listPhotos)
+                .build();
+        shareDialog.show(sharePhotoContent);
+    }
+
+    private void shareLink() {
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            shareLinkContent = new ShareLinkContent.Builder()
+                    .setContentUrl(Uri.parse("https://dantri.com.vn/xa-hoi/thu-tuong-yeu-cau-trien-khai-thi-hanh-luat-bao-ve-bi-mat-nha-nuoc-20190222081241411.htm"))
+                    .setQuote("Test quote")
+                    .build();
+            shareDialog.show(shareLinkContent);
+        }
     }
 }
