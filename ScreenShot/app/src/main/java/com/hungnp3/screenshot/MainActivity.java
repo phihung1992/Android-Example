1e436f0c88b4fb4d81cc8862e97fb91cc7bf6747
hung.nguyen1 <hung.nguyen1@ntq-solution.com.vn>
2019-03-12 14:02:40 +0700

    update sample

--- ScreenShot/app/src/main/java/com/hungnp3/screenshot/MainActivity.java
+++ ScreenShot/app/src/main/java/com/hungnp3/screenshot/MainActivity.java
@@ -1,7 +1,18 @@
 package com.hungnp3.screenshot;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_take_shoot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screenShot();
+            }
+        });
+    }
+
+    public void screenShot() {
+        // Need  Request Permisssion Android 6.0
+
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        String formatedString = new SimpleDateFormat("yyyy-MM-dd_hh:mm:ss").format(now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + formatedString + ".jpg";
+
+            // create bitmap screen capture
+            View rootView = getWindow().getDecorView().getRootView();
+            // If you want to use this on fragment view then use:
+            // View rootView = getActivity().getWindow().getDecorView().getRootView();
+
+            rootView.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(rootView.getDrawingCache());
+            rootView.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or DOM
+            e.printStackTrace();
+        }
+    }
+
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
     }
 }
